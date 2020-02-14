e6de946fd5fa1bedd7fa1b2090bddd8a7386911c
Monreal Tolmo <jrml@gft.com>
2020-02-14 10:30:10 +0100

    cambios en setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version='0.2',
     author='Matthias Feys',
     author_email='matthiasfeys@gmail.com',
-    #install_requires=['tensorflow==1.8.0',
-    #                 'tensorflow-transform==0.8.0'],
+    install_requires=['tensorflow==1.15.0',
+                     'tensorflow-transform'],
     packages=find_packages(exclude=['data']),
     description='tf.Transform demo for digital twin',
     url='https://github.com/Fematich/tftransform-demo/'
